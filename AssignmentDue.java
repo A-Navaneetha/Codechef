fd8a04bed5bb0578fcea17d23889943ac18fc761
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-28 10:40:15 +0530

    Add files via upload

---
+++ AssignmentDue.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AssignmentDue
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X<=Y)
+		System.out.print("yes");
+		else
+		System.out.println("no");
+
+	}
+}
