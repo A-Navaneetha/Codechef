67f9dba40ce3ba510d0dde9c1097c1e0b6ee2a1c
A.Navaneetha <anavaneethasankar@gmail.com>
2025-02-28 10:35:48 +0530

    Add files via upload

---
+++ TimePenalty.java
@@ -0,0 +1,17 @@
+
+    import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class TimePenalty {
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int X,Y;
+		X=sc.nextInt();
+		Y=sc.nextInt();
+		int result=X+(Y*10);
+		System.out.println(result);
+		
+
+	}
+}
